27ce78823ac8196dd9a98748e68898ca3ad50531
kernel1024 <kernel1024@users.noreply.github.com>
2013-03-14 23:50:00 +0000

    Indexer query parsing bugfixes.

--- recollsearch.cpp
+++ recollsearch.cpp
@@ -15,8 +15,8 @@ void CRecollSearch::doSearch(const QString &qr, int maxLimit)
     if (working) return;
     working = true;
     QProcess recoll;
-    recoll.start("recoll",QStringList() << "-t" << "-n" << QString::number(maxLimit)
-                 << "-b" << "-q" << qr);
+    recoll.start("recoll",QStringList() << "-n" << QString::number(maxLimit)
+                 << "-b" << "-a" << "-t" << "-q" << qr);
     recoll.waitForFinished(60000);
     while (!recoll.atEnd()) {
         QString fname = QString::fromUtf8(recoll.readLine()).trimmed();
